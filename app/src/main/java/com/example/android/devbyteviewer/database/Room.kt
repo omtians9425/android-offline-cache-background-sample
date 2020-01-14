073c2b1ccc07543cf7cfff04b98d5eeb2db135da
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-14 14:26:18 +0900

    change viewmodel to use repository

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -35,19 +35,20 @@ interface VideoDao {
 @Database(entities = [DatabaseVideo::class], version = 1)
 abstract class VideosDatabase : RoomDatabase() {
     abstract val videoDao: VideoDao
+}
 
-    private lateinit var INSTANCE: VideosDatabase
 
-    fun getDatabase(context: Context): VideosDatabase {
-        synchronized(VideosDatabase::class.java) {
-            if (!::INSTANCE.isInitialized) {
-                INSTANCE = Room.databaseBuilder(
-                    context.applicationContext,
-                    VideosDatabase::class.java,
-                    "videos"
-                ).build()
-            }
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(
+                context.applicationContext,
+                VideosDatabase::class.java,
+                "videos"
+            ).build()
         }
-        return INSTANCE
     }
+    return INSTANCE
 }
