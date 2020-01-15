f6aca35374521c6ef5eb9a8c93ea63e9d605b6d1
小俣幸之助 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-15 14:33:08 +0900

    implement worker

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,27 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params)  {
+    override suspend fun doWork(): Payload {
+        // To accomplish inject dependencies, need to use AssistedInject.
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideo()
+            Payload(Result.SUCCESS)
+        } catch (e: HttpException) {
+            Payload(Result.FAILURE)
+        }
+
+    }
+
+}
